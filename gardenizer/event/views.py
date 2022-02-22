1c9a0df662acff12444f9e78a0bd25aaf4a7c557
le gulluche <plegulluche@gmail.com>
2022-02-22 19:47:39 +0100

    add_event template with form and corresponding view added

--- gardenizer/event/views.py
+++ gardenizer/event/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Category, City, Evenement
+from account.models import Account
 
-# Create your views here.
+def add_event_view(request):
+    """
+    View displaying a form to add events to the database.
+    """
+    message = ""
+    if request.method == 'POST':
+        title = request.POST.get("title")
+        start_date = request.POST.get("startdate")
+        end_date = request.POST.get("enddate")
+        description = request.POST.get("description")
+        category_id = request.POST.get("category")
+        city_id = request.POST.get("city")
+        
+        new_event = Evenement()
+        new_event.title = title
+        new_event.event_start = start_date
+        new_event.event_end = end_date
+        new_event.description = description
+        new_event.category = Category.objects.get(id=int(category_id))
+        new_event.city = City.objects.get(id=int(city_id))
+        new_event.user = Account.objects.get(pk=request.user.id)
+        new_event.save()
+    
+    categories = Category.objects.all()
+    cities = City.objects.all()
+    context = {"categories":categories, "cities":cities}
+    
+    return render(request, "event/add_event_form.html",context)
--- gardenizer/gardenizer/urls.py
+++ gardenizer/gardenizer/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('website.urls')),
+    path('', include('event.urls')),
 ]
