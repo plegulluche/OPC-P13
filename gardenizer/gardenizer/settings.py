d06112395c1fd69154746ff0238574dcf90550e0
le gulluche <plegulluche@gmail.com>
2022-03-23 00:06:39 +0100

    adding django key to dotenv

--- gardenizer/gardenizer/settings.py
+++ gardenizer/gardenizer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ng4z3g2!ln+xg&9i8&hbi)f7a2%i0lxx)t9firvd@vl9g%)5#v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
