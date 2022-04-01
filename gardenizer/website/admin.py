6a2d1a14dd5ff35e99421fe90ec0de0f666141e3
le gulluche <plegulluche@gmail.com>
2022-04-01 05:00:10 +0200

    updating requirements and adding Meteodata model to admin panel

--- gardenizer/website/admin.py
+++ gardenizer/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from account.models import Account
 from event.models import Category, City, Customer, Evenement
+from meteo.models import MeteoData
 
 # Register your models here.
 admin.site.register(Account)
@@ -8,6 +9,7 @@ admin.site.register(Category)
 admin.site.register(City)
 admin.site.register(Customer)
 admin.site.register(Evenement)
+admin.site.register(MeteoData)
 
 
 class UserAdmin(admin.ModelAdmin):
